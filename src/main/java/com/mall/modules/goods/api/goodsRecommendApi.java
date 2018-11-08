419c9116aab83490ba8b7ef859f71ba25bb81d9d
汉三胡 <luckluckstyle@gmail.com>
2018-11-08 09:07:39 +0800

    新增 商品分享码功能

--- src/main/java/com/mall/modules/goods/api/goodsRecommendApi.java
+++ src/main/java/com/mall/modules/goods/api/goodsRecommendApi.java
@@ -17,12 +17,14 @@ import com.mall.modules.sys.entity.UserVo;
 import com.mall.modules.sys.utils.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
